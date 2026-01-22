c1df27126a9261c384dd2fa4c62827a51e4e9d75
pandrr <tom@tomkom.net>
2026-01-22 11:12:39 +0100

    correct version...

--- rapier/webpack.config.js
+++ rapier/webpack.config.js
@@ -1,33 +1,9 @@
 const path = require('path');
 
-module.exports = [
-  {
-    entry: './index.js',
-    mode: 'production',
-    devtool: 'source-map',
-  
-    output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: 'rapierpkg.min.js',
-  
-      library: {
-        name: 'rapierpkg',
-        type: 'umd',
-      },
-    },
-  },{
-    entry: './index.js',
-    mode: 'development',
-  
-    output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: 'rapierpkg.js',
-  
-      library: {
-        name: 'rapierpkg',
-        type: 'umd',
-      },
-    },
-  }]
-
-
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'rapier.js',
+  },
+};
