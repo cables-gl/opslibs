09eb98f2621303582cdec1d17c06164eb92681b9
Stephan Maihöfer <stephan@undev.de>
2026-01-30 09:25:18 +0100

    clean

--- rapier/webpack.config.js
+++ rapier/webpack.config.js
@@ -5,16 +5,21 @@ const metaUrl = new URL(".", import.meta.url);
 const __dirname = fileURLToPath(metaUrl.href);
 
 export default () => {
-    return {
-        entry: './src/index.js',
-        mode: "production",
-        performance: {
-            maxEntrypointSize: 2500000,
-            maxAssetSize: 2500000
-        },
-        output: {
-            path: path.resolve(__dirname, 'dist'),
-            filename: 'rapier.js',
+    return [
+        {
+            entry: './src/index.js',
+            mode: "production",
+            performance: {
+                maxEntrypointSize: 2500000,
+                maxAssetSize: 2500000
+            },
+            experiments: {
+                asyncWebAssembly: true
+            },
+            output: {
+                path: path.resolve(__dirname, 'dist'),
+                filename: 'rapier.js',
+            }
         }
-    }
-};
+    ];
+}
