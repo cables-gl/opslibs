33b31425ca420dfe4e076ebb23d3300ad49b29a3
steam0r <stephan@undev.de>
2026-05-19 15:11:59 +0200

    remove mikktspace

--- mikktspace/src/index.js
+++
@@ -1,6 +0,0 @@
-import { generateTangents } from 'mikktspace';
-
-// const tangents = generateTangents(positions, normals, uvs); // → Float32Array
-
-
-window.MIKKTSPACE={generateTangents:generateTangents};
